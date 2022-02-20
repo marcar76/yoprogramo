44f539f2e45775a99a177afb74234ae67de5bc13
Mario Carera <marcar76@gmail.com>
2022-02-20 13:12:31 -0300

    Add task list

--- 22-angular-1/task-list/src/app/app.module.ts
+++ 22-angular-1/task-list/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BotonComponent } from './components/boton/boton.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BotonComponent
+    BotonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
